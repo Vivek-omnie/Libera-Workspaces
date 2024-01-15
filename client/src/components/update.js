3be8b5ae46941043d0c06b0e85a66ca7edf726e3
Vivek Doshi <vivekdoshi@omniesolutions.com>
2024-01-15 19:45:39 +0530

    Update update.js

--- client/src/components/update.js
+++ client/src/components/update.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
 import "./create.css";
 // import Switch from "@mui/material/Switch";
@@ -10,6 +10,7 @@ export const Update = () => {
   const [your_name, setyour_name] = useState("");
   const [isActive] = useState(false);
   const [response, setResponse] = useState("");
+  const submitButtonRef = useRef(null);
 
   const postData = async (event) => {
     event.preventDefault();
@@ -28,6 +29,21 @@ export const Update = () => {
       console.error("Error posting data:", error);
     }
   };
+
+  useEffect(() => {
+    const listener = (event) => {
+      if (event.code === "Enter" || event.code === "NumpadEnter") {
+        console.log("Enter key was pressed. Run your function.");
+        event.preventDefault();
+        // postData()
+        submitButtonRef.current.click();
+      }
+    };
+    document.addEventListener("keydown", listener);
+    return () => {
+      document.removeEventListener("keydown", listener);
+    };
+  }, []);
 
   return (
     <form onSubmit={postData}>
@@ -53,7 +69,12 @@ export const Update = () => {
         </label> */}
       </div>
       <div className="submit-buttons">
-        <input type="submit" value="Back" onClick={() => navigate("/")} />
+        <input
+          ref={submitButtonRef}
+          type="submit"
+          value="Back"
+          onClick={() => navigate("/")}
+        />
         <input type="submit" value="Submit" />
       </div>
       {response && <p> {"WorkSpace Updated"}</p>}
