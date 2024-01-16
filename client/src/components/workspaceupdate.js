e22ade2134ff6dcb9cdcec2c0d3da11017166424
Vivek Doshi <vivekdoshi@omniesolutions.com>
2024-01-16 12:16:13 +0530

    Update workspaceupdate.js

--- client/src/components/workspaceupdate.js
+++ client/src/components/workspaceupdate.js
@@ -31,9 +31,9 @@ export const Updatefull = () => {
   useEffect(() => {
     const listener = (event) => {
       if (event.code === "Enter" || event.code === "NumpadEnter") {
-        console.log("Enter key was pressed. Run your function.");
+   
         event.preventDefault();
-        // postData()
+    
         submitButtonRef.current.click();
       }
     };
